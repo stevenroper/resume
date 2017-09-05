707c61f6e88f68168b11578eb5fc2f8eedf20076
Steven Roper <steven.m.roper@gmail.com>
2017-09-05 16:33:09 -0600

    Creates basic Job_Card.

---
+++ app/Resume_Container/components/Job_Card/index.js
@@ -0,0 +1,138 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import colors from 'config/colors';
+
+
+class Job_Card extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false,
+    };
+  }
+
+  getStyles() {
+    return {
+      container: {
+        margin: '10px 15px',
+        boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.35)',
+        padding: '10px',
+        cursor: 'pointer',
+        borderTop: `5px solid ${this.props.currentJob ? colors.mintGreen : colors.yellow}`,
+        borderRadius: '2px',
+      },
+      jobTitle: {
+        fontSize: '2em',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        marginBottom: '-8px',
+        color: colors.blue,
+      },
+      companyName: {
+        fontSize: '1.2em',
+      },
+      divider: {
+        border: 0,
+        borderTop: `1px solid ${colors.blue}`
+      },
+      titleBar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      },
+      dates: {
+        fontSize: '1.3em',
+      },
+      techStackContainer: {
+        width: '100%',
+        height: '40px',
+        display: 'flex',
+      },
+      techStackTitle: {
+        textAlign: 'center',
+        fontSize: '1.2em',
+        marginBottom: '10px',
+        color: colors.blue,
+      }
+    };
+  }
+
+  techStackCompose() {
+    const styles = this.getStyles();
+    let colorValues = Object.keys(colors).map( key => colors[key]);
+
+    let techDivs = Object.keys(this.props.techStack).map( (key, index) => {
+      let techStackStyles = {
+        height: '100%',
+        width: this.props.techStack[key],
+        backgroundColor: colorValues[index],
+        color: '#fff',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '0 10px',
+      };
+      return (
+        <div
+          key={key}
+          style={techStackStyles}>
+            {key}
+        </div>
+      );
+    });
+
+    return (
+      <div>
+        <div style={styles.techStackTitle}>Tech Stack</div>
+        <div style={styles.techStackContainer}>
+
+          {techDivs}
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const styles = this.getStyles();
+
+    return (
+      <div
+        style={styles.container}
+        onClick={() => this.setState({ expanded: !this.state.expanded })}>
+        <div style={styles.titleBar}>
+          <div>
+            <div style={styles.jobTitle}>{this.props.jobTitle}</div>
+            <div style={styles.companyName}>{this.props.company}</div>
+          </div>
+          <div style={styles.dates}>{this.props.dates}</div>
+        </div>
+        <hr style={styles.divider} />
+        <div>
+          { this.state.expanded
+            ? <div>
+                {this.props.longDescription}
+                <hr style={styles.divider} />
+                {this.techStackCompose()}
+              </div>
+            : this.props.shortDescription
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+Job_Card.propTypes = {
+  jobTitle: PropTypes.string.isRequired,
+  company: PropTypes.string.isRequired,
+  shortDescription: PropTypes.node.isRequired,
+  currentJob: PropTypes.bool,
+  dates: PropTypes.node.isRequired,
+  longDescription: PropTypes.node.isRequired,
+  techStack: PropTypes.object,
+};
+
+export default Job_Card;
--- app/Resume_Container/index.js
+++ app/Resume_Container/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Job_Card from './components/Job_Card'
 import colors from 'config/colors';
 
 class Resume_Container extends React.Component {
@@ -28,7 +29,12 @@ class Resume_Container extends React.Component {
         backgroundColor: colors.blue,
         color: '#fff',
         fontSize: '1.3em',
-      }
+        marginBottom: '20px',
+      },
+      reverse: {
+        unicodeBidi: 'bidi-override',
+        direction: 'rtl',
+      },
     };
   }
 
@@ -41,9 +47,35 @@ class Resume_Container extends React.Component {
           <span style={styles.name}>STEVEN ROPER</span>
         </div>
         <div style={styles.contactInfo}>
-          <span>555.555.5555</span>
-          <span>REPLACEME@gmail.com</span>
-          <span>github.com/stevenroper</span>
+          <span style={styles.reverse}>6858.376.108</span>
+          <span style={styles.reverse}>moc.liamg@repor.m.nevets</span>
+          <span style={styles.reverse}>repornevets/moc.buhtig</span>
+        </div>
+        <div style={styles.resumeBody}>
+          <Job_Card
+            jobTitle='Web Developer'
+            company='Utah Educational Savings Plan'
+            shortDescription={<span>Developed a new front-end architecture using React and Redux for an aging enterprise solution.</span>}
+            currentJob={true}
+            dates={<span>11/2016 - Current</span>}
+            longDescription={
+              <div>
+                <p>
+                  I joined UESP (Utah Educational Savings Plan) to assist a team in the process of rebuilding an outdated .NET web application.
+                  While occassionally contributing to the REST API, I have primarily focused on the front-end portion of the re-write.
+                </p>
+                <p>
+                  At UESP, I am working as a key contributor to not only the code base, but also to the app's UX design, API structure, and best code practices.
+                  I also have lead efforts in developing front-end build processes, unit testing, and product design.
+                </p>
+              </div>
+            }
+            techStack={{
+              'React': '60%',
+              'Redux': '20%',
+              'Webpack': '10%',
+              'C#/.Net': '10%',
+            }}/>
         </div>
       </div>
     );
