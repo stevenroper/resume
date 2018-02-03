f952c0eb1ecc6e7be958d9f24af683115d3d0828
Steven Roper <steven.m.roper@gmail.com>
2018-02-02 20:01:43 -0700

    Adds responsive styles for Education_Card.

--- app/Resume_Container/components/Education_Card/index.js
+++ app/Resume_Container/components/Education_Card/index.js
@@ -3,51 +3,19 @@ import PropTypes from 'prop-types';
 
 import colors from 'config/colors';
 
+import styles from './styles.css';
+
 
 function Education_Card(props) {
-  let styles = {
-    container: {
-      margin: '20px 0',
-      boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.35)',
-      padding: '10px',
-      borderRadius: '2px',
-    },
-    educationHeader: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    },
-    major: {
-      fontSize: '2em',
-      fontWeight: 'bold',
-      textTransform: 'uppercase',
-      marginBottom: '-8px',
-      color: colors.blue,
-    },
-    school: {
-      fontSize: '1.2em',
-    },
-    dates: {
-      fontSize: '1.3em',
-    },
-    divider: {
-      border: 0,
-      borderTop: '1px solid rgba(0, 0, 0, 0.15)',
-    },
-  };
   return (
-    <div style={styles.container}>
-      <div style={styles.educationHeader}>
-        <div>
-          <div style={styles.major}>{props.major}</div>
-          <div style={styles.school}>{props.school}</div>
-        </div>
-        <div style={styles.dates}>
-          {props.dates}
-        </div>
+    <div className={styles.container}>
+      <div className={styles.educationHeader}>
+        <div className={styles.major}>{props.major}</div>
+        <div className={styles.school}>{props.school}</div>
+        <div className={styles.dates}>{props.dates}</div>
       </div>
-      <hr style={styles.divider} />
-      <div style={styles.description}>
+      <hr className={styles.divider} />
+      <div className={styles.description}>
         {props.description}
       </div>
     </div>
