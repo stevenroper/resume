9ed35e670722f9ae86077fada7d9ad68f42188f3
Steven Roper <steven.m.roper@gmail.com>
2018-02-02 20:08:29 -0700

    Moves (some) styles into css.

--- app/Resume_Container/components/Skills_Tag/index.js
+++ app/Resume_Container/components/Skills_Tag/index.js
@@ -3,21 +3,19 @@ import PropTypes from 'prop-types';
 
 import colors from 'config/colors';
 
+import styles from './styles.css';
+
 
 function Skills_Tag(props) {
-  const styles = {
+  const inlineStyles = {
     container: {
-      margin: '7px 0',
-      boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.35)',
-      padding: '10px 20px',
       backgroundColor: props.color || '#fff',
       color: props.color ? '#fff' : colors.blue,
-      fontSize: '1.1em',
     },
   };
-
+  
   return (
-    <div style={styles.container}>
+    <div className={styles.container} style={inlineStyles.container}>
       {props.tagName}
     </div>
   );
